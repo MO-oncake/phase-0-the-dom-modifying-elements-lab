e687139b0cf32345ee01f15f9f19f3072ae65931
William Gitau <dontysh@gmail.com>
2024-04-10 20:53:38 +0300

    a

--- index.js
+++ index.js
@@ -6,4 +6,3 @@ const newHeader = document.createElement(`h1`); // creates a new header that poi
 
 newHeader.id = `victory`; // sets the id of the new header to 'victory'.
 
-newHeader.innerHTML = `WILL is the champion`; // sets the innerHTML (content text) of the new header to 'YOUR-NAME is the champion'.
