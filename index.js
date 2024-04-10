fdcd718da15d78ca76046e6e57f119e062b64528
William Gitau <dontysh@gmail.com>
2024-04-10 20:46:25 +0300

    adds code that show html manilulation through javascript.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector(`#main`);
+main.remove(); // removes the main element.
+
+const newHeader = document.createElement(`h1`); // creates a new header that points to an h1 node.
+
+newHeader.id = `victory`; // sets the id of the new header to 'victory'.
+
+newHeader.innerHTML = `WILL is the champion`; // sets the innerHTML (content text) of the new header to 'YOUR-NAME is the champion'.
